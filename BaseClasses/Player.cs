42ad31da95f94d3af4e825d4d7b24e877af6bb77
Khris <hrek1994@gmail.com>
2025-02-14 22:08:48 -0500

    [FT-0001]Improved perfomance, fixed css styles

--- BaseClasses/Player.cs
+++ BaseClasses/Player.cs
@@ -3,31 +3,15 @@
     public class Player
     {
 
-        public string Id { get; set; }
-        public string? Name { get; set; }
+        public string? NiclName { get; set; }
 
-        public string? Country { get; set; }
-
-        //public string? CourtType { get; set; }
         public CourtType CourtTypeEnum { get; set; }
         public enum CourtType
         {
             grass = 0,
             clay = 1,
             hard = 2
-
         }
 
-        public Player(string id)
-        {
-            Id = GenerateCustomID();
-
-        }
-
-
-        private string GenerateCustomID()
-        {
-            return Guid.NewGuid().ToString().Substring(0, 8);
-        }
     }
 }
